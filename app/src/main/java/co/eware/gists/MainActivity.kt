2645cdeb56b4cf0371ad1fb08a6184f30f60c0f4
Ahmed Ibrahim <a.elgammal112@gmail.com>
2020-12-21 13:50:24 +0300

    [+] setup base usage to be shared between features

--- app/src/main/java/co/eware/gists/MainActivity.kt
+++ app/src/main/java/co/eware/gists/MainActivity.kt
@@ -2,10 +2,19 @@ package co.eware.gists
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import co.eware.gists.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.btnExample.setOnClickListener { action() }
+    }
+
+    private fun action() {
+
     }
 }
