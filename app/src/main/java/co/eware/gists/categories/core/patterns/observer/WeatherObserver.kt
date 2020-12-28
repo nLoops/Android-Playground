1ee8140f5b2bf32b736c0dca1aaa7f806ade8bc6
Ahmed Ibrahim <a.elgammal112@gmail.com>
2020-12-28 11:43:01 +0300

    [-] Remove unused code.

--- app/src/main/java/co/eware/gists/categories/core/patterns/observer/WeatherObserver.kt
+++
@@ -1,8 +0,0 @@
-package co.eware.gists.categories.core.patterns.observer
-
-/**
- * Created by Ahmed Ibrahim on 23,December,2020
- */
-interface WeatherObserver {
-    fun update(weatherType: WeatherType)
-}
